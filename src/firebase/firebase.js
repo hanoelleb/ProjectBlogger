ebe6958341fb6ef2224eb0be67429d851f6e7780
Hannah <hanoelleb@gmail.com>
2020-06-29 21:05:43 -0700

    Adding server for deployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,8 @@ import app from 'firebase/app';
 import auth from 'firebase/auth';
 import 'firebase/database';
 
+console.log(process.env.REACT_APP_API_KEY);
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
